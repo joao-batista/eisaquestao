05e70bbb475a60d83f25a92e05f8c6450a3539f3
joao-batista <joao.stinfo@gmail.com>
2018-09-04 12:24:26 -0300

    Criando os Modelos

--- questoes/models.py
+++ questoes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Disciplina(models.Model):
+    descricao = models.CharField(max_length = 50, unique = True)
+
+class Ano(models.Model):
+    descricao = models.CharField(max_length = 4, unique = True)
+
+class Banca(models.Model):
+    descricao = models.CharField(max_length = 50, unique = True)
+
+class Nivel(models.Model):
+    descricao = models.CharField(max_length = 50, unique = True)
+
+class Orgao(models.Model):
+    descricao = models.CharField(max_length = 50, unique = True)
+
+
+class Pergunta(models.Model):
+    texto = models.TextField()
+    respondida = models.BooleanField(default = False)
+    correta = models.BooleanField(default = False)
+    disciplina = models.ForeignKey('Disciplina', on_delete = models.CASCADE, related_name = 'pergunta')
+    ano = models.ForeignKey('Ano', on_delete = models.CASCADE, related_name = 'pergunta')
+    orgao = models.ForeignKey('Orgao', on_delete = models.CASCADE, related_name = 'pergunta')
+    nivel = models.ForeignKey('Nivel', on_delete = models.CASCADE, related_name = 'pergunta')
+    banca = models.ForeignKey('Banca', on_delete = models.CASCADE, related_name = 'pergunta')
+
+class Alternativa(models.Model):
+    correta = models.BooleanField(default = False)
+    selecionada = models.BooleanField(default = False)
+    pergunta = models.ForeignKey('Pergunta', on_delete = models.CASCADE, related_name = 'alternativa')
