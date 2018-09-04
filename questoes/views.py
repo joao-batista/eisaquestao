bc1d80010b785774c5fae28de4ad060b87ce104e
joao-batista <joao.stinfo@gmail.com>
2018-09-04 19:08:42 -0300

    View Pergunta

--- questoes/views.py
+++ questoes/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PerguntaForm, AlternativaForm
+from .models import Pergunta, Alternativa
 
 def index(request):
     return render(request, 'index.html')
 
 def pergunta_inserir(request):
-    return render(request, 'pergunta_form.html')
+    form_pergunta = PerguntaForm(request.POST)
+    form_alternativas = [AlternativaForm(request.POST, prefix = str(i)) for i in range(1, 6)]
+
+    if (form_pergunta.is_valid() and all(form_alternativa.is_valid() for form_alternativa in form_alternativas)):
+        pergunta = form_pergunta.save()
+        for form_alternativa in form_alternativas:
+            alternativa = form_alternativa.save(commit=False)
+            alternativa.pergunta = pergunta
+            alternativa.save()
+        redirect('index')
+    return render(request, 'pergunta_form.html', { 'form_pergunta' : form_pergunta,  'form_alternativas': form_alternativas })
