eec06bba53fd5a098278a90bb173b33953a9a282
joao-batista <joao.stinfo@gmail.com>
2018-09-04 17:26:42 -0300

    Url Pergunta

--- questoes/urls.py
+++ questoes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name = 'index'),
+    path('pergunta/inserir', views.pergunta_inserir, name = 'pergunta_inserir'),
 ]
--- questoes/views.py
+++ questoes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def pergunta_inserir(request):
+    return render(request, 'pergunta_form.html')
