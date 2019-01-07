64ee156a88cb7ce07db30b25db3d6ae5ce2ef4d3
joao-batista <joao.stinfo@gmail.com>
2019-01-07 12:55:59 -0300

    alteração na paginação

--- questoes/views.py
+++ questoes/views.py
@@ -39,21 +39,36 @@ def pergunta_atualizar(request, id):
 
 
 def filtrar(request):
-    form = FiltroForm(request.POST)
+
+    if not request.method == 'POST':
+        if 'search-persons-post' in request.session:
+            request.POST = request.session['search-persons-post']
+            request.method = 'POST'
+
+    if request.method == 'POST':
+        request.session['search-persons-post'] = request.POST
+        form = FiltroForm(request.POST)
 
     if form.is_valid():
         data = form.cleaned_data
         query = Q()
+        
         if data.get('disciplina'):
             query.add(Q(disciplina = data.get('disciplina')), Q.AND)
+            request.session['disciplina'] = str(data.get('disciplina'))
         if data.get('banca'):
             query.add(Q(banca = data.get('banca')), Q.AND)
+            request.session['banca'] = str(data.get('banca'))
         if data.get('ano'):
             query.add(Q(ano = data.get('ano')), Q.AND)
+            request.session['ano'] = str(data.get('ano'))
         if data.get('nivel'):
             query.add(Q(nivel = data.get('nivel')), Q.AND)
+            request.session['nivel'] = str(data.get('nivel'))
         if data.get('orgao'):
             query.add(Q(orgao = data.get('orgao')), Q.AND)
+            request.session['orgao'] = str(data.get('orgao'))
+
         perguntas = Pergunta.objects.filter(query).all()
     return render_index(request, perguntas)
 
@@ -91,4 +106,4 @@ def render_index(request, perguntas):
     page = request.GET.get('page')
     perguntas = paginator.get_page(page)
 
-    return render(request, 'index.html', { 'perguntas' : perguntas, 'filtro_form' : FiltroForm() })
+    return render(request, 'index.html', { 'perguntas' : perguntas, 'filtro_form' : FiltroForm(request.POST) })
